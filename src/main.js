37b1bf58b9ac2c9dc0a5972166013c9bb7ace5a1
Ashmin Karki <ashmin.karki1996@gmail.com>
2019-10-16 13:42:53 +0545

    Calendar Events

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VueTextareaAutosize from "vue-textarea-autosize";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+Vue.use(VueTextareaAutosize);
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAHN0HEVe8XVGA9k_SkjNNrKh2hMOGSC9o",
+  authDomain: "vue-calendar-57788.firebaseapp.com",
+  databaseURL: "https://vue-calendar-57788.firebaseio.com",
+  projectId: "vue-calendar-57788",
+  storageBucket: "vue-calendar-57788.appspot.com",
+  messagingSenderId: "1093394646175",
+  appId: "1:1093394646175:web:74b349d5e364880b544eaa",
+  measurementId: "G-0TG940LPFJ"
+});
+
+export const db = firebase.firestore();
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  render: h => h(App)
+}).$mount("#app");
